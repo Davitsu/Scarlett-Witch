2ef03d14c374c21029ecfe98204ac813cbc44c87
Davitsu <david.kitsu@gmail.com>
2015-11-07 13:26:59 +0100

    License

--- scarlett/src/game.asm
+++ scarlett/src/game.asm
@@ -1,5 +1,20 @@
-; Davitsu (David Giron) 2015
-; Scarlett Witch
+;;-----------------------------LICENSE NOTICE------------------------------------
+;;  This file is part of Scarlett Witch: A Game Boy and Game Boy Color Game
+;;  Copyright (C) 2015 David Giron Jareno (@Davitsu - david.kitsu@gmail.com)
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+;;
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;------------------------------------------------------------------------------
 
 INCLUDE "hardware.inc"
 INCLUDE "header.inc"
